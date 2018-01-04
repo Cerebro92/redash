1f2b7db92ef62128e7a5e1e6c4b4e0873e9b49cc
Neeraj Gahlot <neeraj.gahlot@delhivery.com>
2018-01-04 17:27:39 +0530

    Added functionality to push reports to custom s3 bucket path

--- taskworker/tasks.py
+++ taskworker/tasks.py
@@ -17,31 +17,46 @@ class PushToS3Task(task.Task):
 
     def run_task(self, query_id, api_key, **params):
         try:
-            mail_to = params.pop('emails', None)
             print params
+            mail_to = params.pop('emails', None)
+            s3_path = params.pop('_s3_path', None)
+
             raw_payload = get_query_csv(query_id, api_key, **params)
 
             key = '{}/report_{}.csv'.format(str(query_id),
                     datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
 
+            s3_bucket, s3_path_prefix = s3_path.split('/', 1)
+            if not s3_bucket:
+                s3_bucket = settings.REPORTS_S3_BUCKET
+
+            if s3_path_prefix:
+                key = '{}/{}'.format(s3_path_prefix, key)
+
+            print s3_bucket, key
+
             # push to S3
-            push_data_to_s3(s3_client, raw_payload, settings.REPORTS_S3_BUCKET, key)
+            push_data_to_s3(s3_client, raw_payload, s3_bucket, key)
 
-            # generate presigned URL
-            url = generate_presigned_s3_url(s3_read_client, settings.REPORTS_S3_BUCKET,
-                    key, settings.S3_URL_EXPIRY_TIME, 'GET')
+            # generate s3 signed URL, if emails provided
+            if mail_to:
+                # generate presigned URL
+                url = generate_presigned_s3_url(s3_read_client, s3_bucket,
+                        key, settings.S3_URL_EXPIRY_TIME, 'GET')
 
-            EMAIL_TO = _fix_recipients_list(mail_to)
-            EMAIL_CC = []
-            EMAIL_SUBJECT = params.pop('name', settings.EMAIL_SUBJECT)
-            is_sent = send_email(ses_client, settings.EMAIL_FROM, EMAIL_TO,
-                            EMAIL_CC, EMAIL_SUBJECT,
-                            settings.EMAIL_BODY.format(url))
+                EMAIL_TO = _fix_recipients_list(mail_to)
+                EMAIL_CC = []
+                EMAIL_SUBJECT = params.pop('name', settings.EMAIL_SUBJECT)
+                is_sent = send_email(ses_client, settings.EMAIL_FROM, EMAIL_TO,
+                                EMAIL_CC, EMAIL_SUBJECT,
+                                settings.EMAIL_BODY.format(url))
 
-            if is_sent:
-                print 'Mail sent'
+                if is_sent:
+                    print 'Mail sent'
+                else:
+                    print 'ISSUE'
             else:
-                print 'ISSUE'
+                print 'Emails not provided'
         except Exception as e:
             print e
             print str(e)
